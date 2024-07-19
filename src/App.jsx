ea932687663704804df0cbb05433db23d707e00d
Abdulloh <abdullohmirzayev7570@gmail.com>
2024-07-19 16:10:08 +0500

    Full-Stack loyiha start

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from 'react-router-dom'
+import { Main, Login, Register } from './components'
+
+const App = () => {
+	return (
+		<div>
+			<Routes>
+				<Route path='/' element={<Main />} />
+				<Route path='/login' element={<Login />} />
+				<Route path='/register' element={<Register />} />
+			</Routes>
+		</div>
+	)
+}
+
+export default App
