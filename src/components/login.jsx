c990f38e2292660cab0db374df54ebd5c94c10e4
Abdulloh <abdullohmirzayev7570@gmail.com>
2024-07-21 15:49:58 +0500

    added Redux. Redux-Toolkit

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,10 +1,20 @@
 import { useState } from 'react'
 import { icon } from '../constants'
 import { Input } from '../ui'
+import { useDispatch, useSelector } from 'react-redux'
+import { loginUserStart } from '../slice/auth'
 
 const Login = () => {
 	const [email, setEmail] = useState('')
 	const [password, setPassword] = useState('')
+	const dispatch = useDispatch()
+	const { isLoading } = useSelector(state => state.auth)
+
+	const loginHandler = e => {
+		e.preventDefault()
+		dispatch(loginUserStart())
+	}
+
 	return (
 		<div className='text-center mt-5'>
 			<main className='form-signin w-25 m-auto'>
@@ -32,10 +42,14 @@ const Login = () => {
 						setState={setPassword}
 					/>
 
-					<button className='btn btn-primary w-100 py-2 mt-2' type='submit'>
-						Login
+					<button
+						className='btn btn-primary w-100 py-2 mt-2'
+						type='submit'
+						onClick={loginHandler}
+						disabled={isLoading}
+					>
+						{isLoading ? 'Loading...' : 'Login'}
 					</button>
-					<p className='mt-5 mb-3 text-body-secondary'>© 2017–2024</p>
 				</form>
 			</main>
 		</div>
